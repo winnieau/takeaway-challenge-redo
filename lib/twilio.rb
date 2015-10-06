41893d2f8d8981752e74cf9070b635e4b7de8ee4
Winifred Au <winifredau1990@gmail.com>
2015-10-06 07:50:26 -0400

    stuff

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -3,13 +3,13 @@
 module Twilio
   def self.send_text_message(customer)
     # Get your Account Sid and Auth Token from twilio.com/user/account
-  account_sid = 'ACd20534bbb6a0aec9f76e2975230e0db6'
-  auth_token = '10de356b4197cb57e2fa960929edc1e7'
+  account_sid = ENV['ACCOUNT_SID']
+  auth_token = ENV['TWILIO_AUTH_TOKEN']
   @client = Twilio::REST::Client.new account_sid, auth_token
 
   message = @client.account.messages.create(:body => "Thank you! Your order was placed and will be delivered before #{@time_order}",
-      :to => "+14438470764",     # Replace with your phone number
-      :from => "+14437013745")   # Replace with your Twilio number
+      :to => ENV['PHONE_NUMBER'],     # Replace with your phone number
+      :from => ENV['TWILIO_NUMBER'])   # Replace with your Twilio number
   puts message.sid
   customer.text_received
   end
